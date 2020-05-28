aa16dd91df33d3448cfb5e5336fd87ca95f72e81
vonewman <vonewman7@gmail.com>
2020-05-28 09:37:30 -0400

    add the Binomial class

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-setup(name='distribution',
-      version='0,1',
+setup(name='distributions',
+      version='0.1',
       description='Gaussian distributions',
       packages=['distributions'],
       zip_safe=False)
